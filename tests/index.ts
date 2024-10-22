47f0e59972d2d5c93484f7573220d28bfd07c38d
KilDesu <91275372+KilDesu@users.noreply.github.com>
2024-10-22 15:52:32 +0200

    feat: add tests

---
+++ tests/index.ts
@@ -0,0 +1,122 @@
+import JSDate from '../src';
+
+const baseInputDates = [
+    {
+        day: '31',
+        month: '12',
+        year: '1985',
+        monthNameShort: {
+            en: 'dec',
+            fr: 'déc',
+        },
+        monthNameLong: {
+            en: 'december',
+            fr: 'décembre',
+        },
+        isValid: true,
+    },
+    {
+        day: '29',
+        month: '02',
+        year: '1999',
+        monthNameShort: {
+            en: 'feb',
+            fr: 'fév',
+        },
+        monthNameLong: {
+            en: 'february',
+            fr: 'février',
+        },
+        isValid: false,
+    },
+    {
+        day: '29',
+        month: '02',
+        year: '2000',
+        monthNameShort: {
+            en: 'feb',
+            fr: 'fév',
+        },
+        monthNameLong: {
+            en: 'february',
+            fr: 'février',
+        },
+        isValid: true,
+    },
+    {
+        day: '01',
+        month: '01',
+        year: '2024',
+        monthNameShort: {
+            en: 'jan',
+            fr: 'janv',
+        },
+        monthNameLong: {
+            en: 'january',
+            fr: 'janvier',
+        },
+        isValid: true,
+    },
+    {
+        day: '26',
+        month: '08',
+        year: '2030',
+        monthNameShort: {
+            en: 'aug',
+            fr: 'août',
+        },
+        monthNameLong: {
+            en: 'august',
+            fr: 'août',
+        },
+        isValid: true,
+    },
+];
+
+export const datesInputsForTests = baseInputDates.map(
+    ({ day, month, year, monthNameShort, monthNameLong, isValid }) => ({
+        inputs: {
+            noSep: `${day}${month}${year}`,
+            noSepYearFirst: `${year}${month}${day}`,
+
+            sepSlashShort: `${day}/${month}/${year.slice(-2)}`,
+            sepSlashLong: `${day}/${month}/${year}`,
+            sepSlashShortYearFirst: `${year.slice(-2)}/${day}/${month}`,
+            sepSlashLongYearFirst: `${year}/${day}/${month}`,
+
+            sepHyphenShort: `${day}-${month}-${year.slice(-2)}`,
+            sepHyphenLong: `${day}-${month}-${year}`,
+            sepHyphenShortYearFirst: `${year.slice(-2)}-${day}-${month}`,
+            sepHyphenLongYearFirst: `${year}-${day}-${month}`,
+
+            sepSpaceMonthNameShortEn: `${day} ${monthNameShort.en} ${year}`,
+            sepSpaceMonthNameShortFr: `${day} ${monthNameShort.fr} ${year}`,
+            sepSpaceMonthNameLongEn: `${day} ${monthNameLong.en} ${year}`,
+            sepSpaceMonthNameLongFr: `${day} ${monthNameLong.fr} ${year}`,
+
+            sepSpaceMonthNameShortEnNoDay: `${monthNameShort.en} ${year}`,
+            sepSpaceMonthNameShortFrNoDay: `${monthNameShort.fr} ${year}`,
+            sepSpaceMonthNameLongEnNoDay: `${monthNameLong.en} ${year}`,
+            sepSpaceMonthNameLongFrNoDay: `${monthNameLong.fr} ${year}`,
+
+            milliseconds: new Date(`${year}-${month}-${day}`).getTime(),
+            date: new Date(`${year}-${month}-${day}`),
+            jsDate: new JSDate(`${day}/${month}/${year}`),
+            noInput: undefined,
+            dateObject: {
+                day: Number(day),
+                month: Number(month),
+                year: Number(year),
+            },
+            objectWithDate: {
+                dateKey: `${day}/${month}/${year}`,
+                otherKey: "My other key I don't care about",
+            },
+        },
+
+        expectedResultString: isValid
+            ? `${day}/${month}/${year}`
+            : 'Date invalide',
+        expectedValidity: isValid,
+    })
+);
---
+++ tests/invalid.test.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect } from 'bun:test';
+import JSDate from '../src';
+
+describe('Invalid Dates', () => {
+    it('Invalid Day', () => {
+        const dateWithInvalidDay = new JSDate('31/11/2019');
+
+        expect(dateWithInvalidDay.isValid).toBe(false);
+        expect(dateWithInvalidDay.error).toMatchObject({
+            hint: `Le jour entré "31" est invalide.`,
+        });
+    });
+
+    it('Invalid Day Leap Year', () => {
+        const dateWithInvalidDay = new JSDate('29/02/2019');
+
+        expect(dateWithInvalidDay.isValid).toBe(false);
+        expect(dateWithInvalidDay.error).toMatchObject({
+            hint: `Le jour entré "29" est invalide car l'année 2019 n'est pas bissextile.`,
+        });
+    });
+
+    it('Invalid Month', () => {
+        const dateWithInvalidMonth = new JSDate('31/13/2019');
+
+        expect(dateWithInvalidMonth.isValid).toBe(false);
+        expect(dateWithInvalidMonth.error).toMatchObject({
+            hint: `Le mois entré "13" est invalide.`,
+        });
+    });
+});
